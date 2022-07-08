ec673c2d39c9d0bedacf4b1face9a96f3fcc3db3
Sveta <sveta.zheleznova.03@mail.ru>
2022-07-09 00:50:57 +0300

    it's working

---
+++ src/HandTracking.py
@@ -0,0 +1,206 @@
+import cv2
+import mediapipe as mp
+import math
+import serial
+import time
+import logging
+import serial.tools.list_ports
+
+class HandDetector:
+    """
+    Finds Hands using the mediapipe library. Exports the landmarks
+    in pixel format. Adds extra functionalities like finding how
+    many fingers are up or the distance between two fingers. Also
+    provides bounding box info of the hand found.
+    """
+
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, minTrackCon=0.5):
+        """
+        :param mode: In static mode, detection is done on each image: slower
+        :param maxHands: Maximum number of hands to detect
+        :param detectionCon: Minimum Detection Confidence Threshold
+        :param minTrackCon: Minimum Tracking Confidence Threshold
+        """
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.minTrackCon = minTrackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode, max_num_hands=self.maxHands,
+                                        min_detection_confidence=self.detectionCon,
+                                        min_tracking_confidence=self.minTrackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
+        self.fingers = []
+        self.lmList = []
+
+    def findHands(self, img, draw=True, flipType=True):
+        """
+        Finds hands in a BGR image.
+        :param img: Image to find the hands in.
+        :param draw: Flag to draw the output on the image.
+        :return: Image with or without drawings
+        """
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        allHands = []
+        h, w, c = img.shape
+        if self.results.multi_hand_landmarks:
+            for handType, handLms in zip(self.results.multi_handedness, self.results.multi_hand_landmarks):
+                myHand = {}
+                ## lmList
+                mylmList = []
+                xList = []
+                yList = []
+                for id, lm in enumerate(handLms.landmark):
+                    px, py, pz = int(lm.x * w), int(lm.y * h), int(lm.z * w)
+                    mylmList.append([px, py, pz])
+                    xList.append(px)
+                    yList.append(py)
+
+                ## bbox
+                xmin, xmax = min(xList), max(xList)
+                ymin, ymax = min(yList), max(yList)
+                boxW, boxH = xmax - xmin, ymax - ymin
+                bbox = xmin, ymin, boxW, boxH
+                cx, cy = bbox[0] + (bbox[2] // 2), \
+                         bbox[1] + (bbox[3] // 2)
+
+                myHand["lmList"] = mylmList
+                myHand["bbox"] = bbox
+                myHand["center"] = (cx, cy)
+
+                if flipType:
+                    if handType.classification[0].label == "Right":
+                        myHand["type"] = "Left"
+                    else:
+                        myHand["type"] = "Right"
+                else:
+                    myHand["type"] = handType.classification[0].label
+                allHands.append(myHand)
+
+                ## draw
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
+                    cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
+                                  (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
+                                  (255, 0, 255), 2)
+                    cv2.putText(img, myHand["type"], (bbox[0] - 30, bbox[1] - 30), cv2.FONT_HERSHEY_PLAIN,
+                                2, (255, 0, 255), 2)
+        if draw:
+            return allHands, img
+        else:
+            return allHands
+
+    def fingersUp(self, myHand):
+        """
+        Finds how many fingers are open and returns in a list.
+        Considers left and right hands separately
+        :return: List of which fingers are up
+        """
+        myHandType = myHand["type"]
+        myLmList = myHand["lmList"]
+        if self.results.multi_hand_landmarks:
+            fingers = []
+            # Thumb
+            if myHandType == "Right":
+                if myLmList[self.tipIds[0]][0] > myLmList[self.tipIds[0] - 1][0]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+            else:
+                if myLmList[self.tipIds[0]][0] < myLmList[self.tipIds[0] - 1][0]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+
+            # 4 Fingers
+            for id in range(1, 5):
+                if myLmList[self.tipIds[id]][1] < myLmList[self.tipIds[id] - 2][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+        return fingers
+
+
+class SerialObject:
+    """
+    Allow to transmit data to a Serial Device like Arduino.
+    Example send $255255000
+    """
+    def __init__(self, portNo=None, baudRate=9600, digits=1):
+        """
+        Initialize the serial object.
+        :param portNo: Port Number.
+        :param baudRate: Baud Rate.
+        :param digits: Number of digits per value to send
+        """
+        self.portNo = portNo
+        self.baudRate = baudRate
+        self.digits = digits
+        connected = False
+        if self.portNo is None:
+            ports = list(serial.tools.list_ports.comports())
+            for p in ports:
+                if "Arduino" in p.description:
+                    print(f'{p.description} Connected')
+                    self.ser = serial.Serial(p.device)
+                    self.ser.baudrate = baudRate
+                    connected = True
+            if not connected:
+                logging.warning("Arduino Not Found. Please enter COM Port Number instead.")
+
+        else:
+            try:
+                self.ser = serial.Serial(self.portNo, self.baudRate)
+                print("Serial Device Connected")
+            except:
+                logging.warning("Serial Device Not Connected")
+
+
+    def sendData(self, data):
+        """
+        Send data to the Serial device
+        :param data: list of values to send
+        """
+        myString = "$"
+        for d in data:
+            myString += str(int(d)).zfill(self.digits)
+        try:
+            self.ser.write(myString.encode())
+            return True
+        except:
+            return False
+
+
+def main():
+    mySerial = SerialObject("/dev/ttyACM0", 9600, 1)
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector(detectionCon=0.8, maxHands=2)
+    while True:
+        # Get image frame
+        success, img = cap.read()
+        # Find the hand and its landmarks
+        hands, img = detector.findHands(img)  # with draw
+        # hands = detector.findHands(img, draw=False)  # without draw
+
+        if hands:
+            # Hand 1
+            hand1 = hands[0]
+            lmList1 = hand1["lmList"]  # List of 21 Landmark points
+            bbox1 = hand1["bbox"]  # Bounding box info x,y,w,h
+            centerPoint1 = hand1['center']  # center of the hand cx,cy
+            handType1 = hand1["type"]  # Handtype Left or Right
+
+            fingers1 = detector.fingersUp(hand1)
+            print(fingers1)
+            mySerial.sendData(fingers1)
+        # Display
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+
+main()
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ extern crate pyo3;
 use pyo3::prelude::*;
 
 fn main() -> PyResult<()> {
-    let code = include_str!("hello.py");
+    let code = include_str!("HandTracking.py");
     Python::with_gil(|py| -> PyResult<()> {
-        PyModule::from_code(py, code, "hello", "hello")?;
+        PyModule::from_code(py, code, "HandTracking", "HandTracking")?;
 
         Ok(())
     })?;
